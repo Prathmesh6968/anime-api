ac515a01bafd85c3368a23106e87723b8e2311bb
Prathmesh6968 <prathmeshcoder69@gmail.com>
2026-03-07 00:57:24 +0530

    Episodes inssue fixed

--- src/app.js
+++ src/app.js
@@ -14,7 +14,7 @@ const express = require("express")
 const cors = require("cors")
 const app = express()
 app.use(cors())
-app.options('*', cors())
+
 /**
  * @desc    All Routes
  * @access  Public
